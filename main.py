c0f24d4c71f4475cc8079e7936856474671eeb16
Franciscus Agnew <agnew_f@hotmail.com>
2025-11-14 10:28:36 -0500

    Added Move and Groups

--- main.py
+++ main.py
@@ -5,24 +5,39 @@ from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(
         f"Screen width: {SCREEN_WIDTH}\n"
         f"Screen height: {SCREEN_HEIGHT}"
     )
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT/2)
+    
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        # player.update(dt)
+        updatable.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+        
+        # player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
         # print(f"dt: {dt}")
 
